a4f9f6871f6cbdf72b9b69079ab7faa85bf80b43
ProGamer MakerRPG <adrien.g@edr31.com>
2024-11-11 21:28:18 +0100

    musiques, gui, button.py

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import pygame as p
 # p setup
 p.init()
 screen = p.display.set_mode((1280, 720))
+p.display.set_caption("jeu_de_rythme")
 # p.mouse.set_visible(False)
 p.mouse.set_cursor(p.SYSTEM_CURSOR_CROSSHAIR)
 clock = p.time.Clock()
 running = True
+
+# Starting the mixer 
+p.mixer.init() 
+main_menu = True  
 
 while running:
     # poll for events
@@ -22,6 +27,13 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill(( 0, 22, 1 ))
+
+    if main_menu == True:
+        main_menu = False
+        p.mixer.music.load("assets\sounds\musics\main menu.mp3") 
+        p.mixer.music.set_volume(0.5)
+        p.mixer.music.play() 
+
 
     # RENDER YOUR GAME HERE
 
