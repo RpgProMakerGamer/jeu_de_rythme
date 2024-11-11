1831486e46b09dfb90ad8da6e9cf0acbfe8c07d8
ProGamer MakerRPG <adrien.g@edr31.com>
2024-11-11 20:15:01 +0100

    amélioration global du main

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame as p
 # p setup
 p.init()
 screen = p.display.set_mode((1280, 720))
+# p.mouse.set_visible(False)
+p.mouse.set_cursor(p.SYSTEM_CURSOR_CROSSHAIR)
 clock = p.time.Clock()
 running = True
 
@@ -13,8 +15,13 @@ while running:
         if event.type == p.QUIT:
             running = False
 
+    # Check for the fullscreen toggle event
+    if event.type == p.KEYDOWN and event.key == p.K_F11:
+        # Toggle fullscreen mode
+        p.display.toggle_fullscreen()
+
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill(( 0, 22, 1 ))
 
     # RENDER YOUR GAME HERE
 
