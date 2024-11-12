aabe3e41dfb60127600c1abb3c7d36dcd7cbd6a2
RpgMaker ProGamer <GamerMakerRpgPro@gmail.com>
2024-11-12 21:55:58 +0100

    ajout des bouton, pas mal de trucs a corriger

--- main.py
+++ main.py
@@ -1,45 +1,61 @@
-import pygame as p
+import pygame
+import button
 
 # p setup
-p.init()
-screen = p.display.set_mode((1280, 720))
-p.display.set_caption("jeu_de_rythme")
-# p.mouse.set_visible(False)
-p.mouse.set_cursor(p.SYSTEM_CURSOR_CROSSHAIR)
-clock = p.time.Clock()
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("jeu_de_rythme")
+# pygame.mouse.set_visible(False)
+pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_CROSSHAIR)
+clock = pygame.time.Clock()
 running = True
 
 # Starting the mixer 
-p.mixer.init() 
+pygame.mixer.init() 
 main_menu = True  
 
+# initialisation de l'image des boutons
+jouer_img = pygame.image.load('Jouer.png').convert_alpha()
+
+# création des instances de boutons
+jouer_btn = button.Button(640, 260, jouer_img, 1.2)
+
 while running:
-    # poll for events
-    # p.QUIT event means the user clicked X to close your window
-    for event in p.event.get():
-        if event.type == p.QUIT:
+    clock.tick(60)  # limits FPS to 60
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             running = False
 
-    # Check for the fullscreen toggle event
-    if event.type == p.KEYDOWN and event.key == p.K_F11:
-        # Toggle fullscreen mode
-        p.display.toggle_fullscreen()
-
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill(( 0, 22, 1 ))
+    if main_menu == False:
+        screen.fill(( 0, 22, 1 ))
+    else :
+        screen.fill(( 78, 22, 1 ))
 
+    # Check for the fullscreen toggle event
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
+        # Toggle fullscreen mode
+        pygame.display.toggle_fullscreen()
+
+    # main menu music player
     if main_menu == True:
         main_menu = False
-        p.mixer.music.load("assets\sounds\musics\main menu.mp3") 
-        p.mixer.music.set_volume(0.5)
-        p.mixer.music.play() 
+        pygame.mixer.music.load("assets\sounds\musics\main menu.mp3") 
+        pygame.mixer.music.set_volume(0.025)
+        pygame.mixer.music.play() 
+
+    if jouer_btn.draw(screen):
+        print('Jouer')
+        main_menu = 'Maybe'
+        # + il faut faire disparaitre le bouton 
 
 
-    # RENDER YOUR GAME HERE
+    # Update the game state
+    pygame.display.update()
 
     # flip() the display to put your work on screen
-    p.display.flip()
+    pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
 
-p.quit()
+pygame.quit()
