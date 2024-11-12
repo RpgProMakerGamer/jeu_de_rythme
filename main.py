69b59616fe4ed44d1a920acc667219dfca1e110c
RpgMaker ProGamer <GamerMakerRpgPro@gmail.com>
2024-11-12 23:26:08 +0100

    remise au propre globale du programme

--- main.py
+++ main.py
@@ -1,61 +1,94 @@
 import pygame
 import button
 
-# p setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-pygame.display.set_caption("jeu_de_rythme")
-# pygame.mouse.set_visible(False)
-pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_CROSSHAIR)
-clock = pygame.time.Clock()
-running = True
 
-# Starting the mixer 
-pygame.mixer.init() 
-main_menu = True  
+#create game window
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
 
-# initialisation de l'image des boutons
-jouer_img = pygame.image.load('Jouer.png').convert_alpha()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Jeu de rythm")
 
-# création des instances de boutons
-jouer_btn = button.Button(640, 260, jouer_img, 1.2)
+#game variables
+game_paused = True
+menu_state = "main"
+main_menu_music = True
 
-while running:
-    clock.tick(60)  # limits FPS to 60
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+#define fonts
+font = pygame.font.SysFont("arialblack", 40)
 
-    # fill the screen with a color to wipe away anything from last frame
-    if main_menu == False:
-        screen.fill(( 0, 22, 1 ))
-    else :
-        screen.fill(( 78, 22, 1 ))
+#define colours
+TEXT_COL = (60, 255, 7)
 
-    # Check for the fullscreen toggle event
-    if event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
-        # Toggle fullscreen mode
-        pygame.display.toggle_fullscreen()
+#load button images
+resume_img = pygame.image.load("images/button_resume.png").convert_alpha()
+options_img = pygame.image.load("images/button_options.png").convert_alpha()
+quit_img = pygame.image.load("images/button_quit.png").convert_alpha()
+video_img = pygame.image.load('images/button_video.png').convert_alpha()
+audio_img = pygame.image.load('images/button_audio.png').convert_alpha()
+keys_img = pygame.image.load('images/button_keys.png').convert_alpha()
+back_img = pygame.image.load('images/button_back.png').convert_alpha()
 
-    # main menu music player
-    if main_menu == True:
-        main_menu = False
-        pygame.mixer.music.load("assets\sounds\musics\main menu.mp3") 
-        pygame.mixer.music.set_volume(0.025)
-        pygame.mixer.music.play() 
+#create button instances
+resume_button = button.Button(304, 125, resume_img, 1)
+options_button = button.Button(297, 250, options_img, 1)
+quit_button = button.Button(336, 375, quit_img, 1)
+video_button = button.Button(226, 75, video_img, 1)
+audio_button = button.Button(225, 200, audio_img, 1)
+keys_button = button.Button(246, 325, keys_img, 1)
+back_button = button.Button(332, 450, back_img, 1)
 
-    if jouer_btn.draw(screen):
-        print('Jouer')
-        main_menu = 'Maybe'
-        # + il faut faire disparaitre le bouton 
+def draw_text(text, font, text_col, x, y):
+  img = font.render(text, True, text_col)
+  screen.blit(img, (x, y))
 
+#game loop
+run = True
+while run:
 
-    # Update the game state
-    pygame.display.update()
+  screen.fill(( 0, 22, 1 ))
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+  # main menu music player
+  if main_menu_music == True:
+    main_menu_music = False
+    pygame.mixer.music.load("assets\sounds\musics\main menu.mp3") 
+    pygame.mixer.music.set_volume(0.025)
+    pygame.mixer.music.play() 
 
+  #check if game is paused
+  if game_paused == True:
+    #check menu state
+    if menu_state == "main":
+      #draw pause screen buttons
+      if resume_button.draw(screen):
+        game_paused = False
+      if options_button.draw(screen):
+        menu_state = "options"
+      if quit_button.draw(screen):
+        run = False
+    #check if the options menu is open
+    if menu_state == "options":
+      #draw the different options buttons
+      if video_button.draw(screen):
+        print("Video Settings")
+      if audio_button.draw(screen):
+        print("Audio Settings")
+      if keys_button.draw(screen):
+        print("Change Key Bindings")
+      if back_button.draw(screen):
+        menu_state = "main"
+  else:
+    draw_text("Press SPACE to pause", font, TEXT_COL, 160, 250)
+
+  #event handler
+  for event in pygame.event.get():
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_SPACE:
+        game_paused = True
+    if event.type == pygame.QUIT:
+      run = False
+
+  pygame.display.update()
 
 pygame.quit()
